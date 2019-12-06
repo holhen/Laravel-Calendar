ebb60f4a1c204548f55e2ce5bc97e1283ea0f884
holhen <holhen@gmail.com>
2019-12-06 23:42:18 +0100

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,12 +23,14 @@ class AuthController extends Controller
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed'
         ]);
+
         $team = Team::where('name', $request->group)->first();
         if(!$team) {
             $team = new Team;
             $team->name = $request->group;
             $team->save();
         }
+
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
@@ -36,6 +38,7 @@ class AuthController extends Controller
             'team_id' => $team->id,
         ]);
         $user->save();
+
         return response()->json([
             'message' => 'Successfully created user!'
         ], 201);
@@ -58,17 +61,20 @@ class AuthController extends Controller
             'password' => 'required|string',
             'remember_me' => 'boolean'
         ]);
+
         $credentials = request(['email', 'password']);
         if(!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
+
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
         if ($request->remember_me)
             $token->expires_at = Carbon::now()->addWeeks(1);
         $token->save();
+
         return response()->json([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
